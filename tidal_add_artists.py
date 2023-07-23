b4d2c43c4c30153aebee37793173ccd68f7ef187
JanuszBIznesu1 <54276954+JanuszBIznesu1@users.noreply.github.com>
2023-07-23 13:41:25 +0200

    Update tidal_add_artists.py
    Add skipped unavailable album artist name.

--- tidal_add_artists.py
+++ tidal_add_artists.py
@@ -1,4 +1,4 @@
-import tidalapi
+import tidalapi, os
 from tqdm import tqdm
 
 session = tidalapi.Session()
@@ -6,7 +6,7 @@ print('Enter your credentials for the account you want to transfer from')
 session.login_oauth_simple()
 uid = session.user.id
 TidalUser = tidalapi.Favorites(session, uid)
-artists = TidalUser.artists()
+albums = TidalUser.albums()
 
 session2 = tidalapi.Session()
 print('Enter your credentials for the account you want to transfer to')
@@ -15,5 +15,10 @@ uid2 = session2.user.id
 TidalUser2 = tidalapi.Favorites(session2, uid2)
 
 print('Copying')
-for i in tqdm(range(len(artists))):
-    TidalUser2.add_artist(artists[i].id)
+for i in tqdm(range(len(albums))):
+    try:
+        TidalUser2.add_album(albums[i].id)
+    except:
+        print('Skipped', albums[i].name, 'by' , albums[i].artist.name)
+
+os.system("pause")
